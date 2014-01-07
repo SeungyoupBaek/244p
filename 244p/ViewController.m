ef961c427d1e4fa52e48928de7d427dadd1bb2d8
SeungyoupBaek <bsy8220@naver.com>
2014-01-08 01:46:28 +0900

    244p 완료했습니다.
    244p 완료했습니다.

--- 244p/ViewController.m
+++ 244p/ViewController.m
@@ -9,14 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *ballImage;
 
 @end
 
 @implementation ViewController
 
+// 회전
+-(IBAction)handleRotate:(UIRotationGestureRecognizer *)gesture{
+    self.ballImage.transform = CGAffineTransformRotate(self.ballImage.transform, gesture.rotation);
+    //연속된 값으로 반환되므로 0으로 초기화
+    [gesture setRotation:0.0];
+}
+
+-(void)handlePan:(UIPanGestureRecognizer *)gesture{
+    CGPoint point = [gesture locationInView:self.view];
+    self.ballImage.center = point;
+}
+
+-(void)handlePinch:(UIPinchGestureRecognizer *)gesture{
+    CGFloat scale = gesture.scale;
+    self.ballImage.transform = CGAffineTransformScale(self.ballImage.transform, scale, scale);
+    //연속된 값으로 반환되므로 1로 초기화
+    [gesture setScale:1.0];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [self.ballImage addGestureRecognizer:pan];
+    
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+    [self.ballImage addGestureRecognizer:pinch];
+    
+    UIRotationGestureRecognizer *rotate = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotate:)];
+    [self.ballImage addGestureRecognizer:rotate];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
